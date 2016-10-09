92c8a2814142f201258702e9049d04d90ebb4038
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2016-10-09 18:41:54 +0200

    start xvfb on travis

--- ops/scripts/clean-migrate-db-env.bat
+++ ops/scripts/clean-migrate-db-env.bat
@@ -1,6 +1,6 @@
 @echo off
 
 pushd %~dp0\..\..
-mvn clean compile -pl tastr-infrastructure flyway:clean flyway:migrate -Dtastrenvironment=%1
+mvn clean compile -pl tastr-infrastructure flyway:clean flyway:migrate -Dtastr.environment=%1
 popd
 
