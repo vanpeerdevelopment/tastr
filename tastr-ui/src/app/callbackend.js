51b75621503d9d8ecef9bc8082a74ab1be3bd57f
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2016-10-14 09:39:18 +0200

    ng2 quickstart

--- tastr-ui/src/app/callbackend.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    $.getJSON("/api/tasting", function (data) {
-        var lis = data
-            .map(function (tasting) {
-                return "<li>" + tasting.naam + "</li>";
-            });
-        $("<ul/>", {html: lis.join("")}).appendTo("body");
-    });
-})();
